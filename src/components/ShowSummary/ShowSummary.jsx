f38566d1f6eb0c0e56650a8804f9e77a725e22cf
sanaullahsabere <you@example.com>
2023-06-07 07:59:21 +0600

    booking from with modal

--- src/components/ShowSummary/ShowSummary.jsx
+++ src/components/ShowSummary/ShowSummary.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 
 const ShowSummary = () => {
   const showSummary = useLoaderData();
+  const [showModal, setShowModal] = useState(false);
 
   const removeTags = (text) => {
     if (text === null || text === "") {
@@ -14,13 +15,63 @@ const ShowSummary = () => {
     return text.replace(/(<([^>]+)>)/gi, "");
   };
 
-  const { summary, name, status, rating, image, language, network , schedule , runtime, type , genres} = showSummary;
+  const {
+    summary,
+    name,
+    status,
+    rating,
+    image,
+    language,
+    network,
+    schedule,
+    runtime,
+    type,
+    genres,
+  } = showSummary;
   const mediumOriginal = image?.original;
   const networkName = network?.name;
   const scheduleTime = schedule?.time;
   const scheduleDays = schedule?.days[0];
   const genresOne = genres[0];
   const genresTwo = genres[1];
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.nameInput.value;
+    const email = form.emailInput.value;
+    const phone = form.phoneInput.value;
+    const showDate = form.dateInput.value;
+    const message = form.messageInput.value;
+
+    const userDetails = {
+      name,
+      email,
+      phone,
+      date: showDate,
+      message,
+      movieName: name, // Assuming the movie name is already present
+    };
+
+    localStorage.setItem("userDetails", JSON.stringify(userDetails));
+
+    form.reset();
+
+    // Close the modal
+    handleModalClose();
+
+    // Display an alert
+    alert("Data stored successfully!");
+  };
 
   return (
     <Container className="py-5">
@@ -34,20 +85,99 @@ const ShowSummary = () => {
             <p className="lead">{removeTags(summary)}</p>
           </div>
         </Col>
-        <Col md={4} className="">
+        <Col md={4} className="mt-4">
           <div className="">
             <span className="fs-1  text-left">Show Info</span>
             <h1 className="mb-4 text-left">{name}</h1>
-            <p className="mb-0"><strong>Network:</strong> {networkName}</p>
-            <p className="mb-0  text-left"><strong>Schedule:</strong> {scheduleDays} at {scheduleTime} ({runtime}min) </p>
-            <p className="mb-0  text-left"><strong>Status:</strong> {status}</p>
-            <p className="mb-0 text-left"><strong>Show Type: </strong>{type}</p>
-            <p className="mb-0 text-left"><strong>Genres: </strong>{genresOne} || {genresTwo}</p>
-            <p className="mb-0"><strong>Rating:</strong> {rating?.average ? rating.average : "No Rating"}</p>
-            <p className="mb-0"><strong>Language:</strong> {language}</p>
+            <p className="mb-0">
+              <strong>Network:</strong> {networkName}
+            </p>
+            <p className="mb-0  text-left">
+              <strong>Schedule:</strong> {scheduleDays} at {scheduleTime} (
+              {runtime}min){" "}
+            </p>
+            <p className="mb-0  text-left">
+              <strong>Status:</strong> {status}
+            </p>
+            <p className="mb-0 text-left">
+              <strong>Show Type: </strong>
+              {type}
+            </p>
+            <p className="mb-0 text-left">
+              <strong>Genres: </strong>
+              {genresOne} || {genresTwo}
+            </p>
+            <p className="mb-0">
+              <strong>Rating:</strong>{" "}
+              {rating?.average ? rating.average : "No Rating"}
+            </p>
+            <p className="mb-0">
+              <strong>Language:</strong> {language}
+            </p>
           </div>
         </Col>
       </Row>
+      <div className="d-flex justify-content-center">
+        <Button variant="outline-primary" onClick={handleModalOpen}>
+          Book Ticket
+        </Button>
+      </div>
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Booking Form - {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleFormSubmit}>
+            <div className="mb-3">
+              <label htmlFor="nameInput" className="form-label">
+                Name
+              </label>
+              <input
+                defaultValue={name}
+                type="text"
+                className="form-control"
+                id="nameInput"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="emailInput" className="form-label">
+                Email
+              </label>
+              <input type="email" className="form-control" id="emailInput" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="phoneInput" className="form-label">
+                Phone
+              </label>
+              <input type="tel" className="form-control" id="phoneInput" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="dateInput" className="form-label">
+                Date
+              </label>
+              <input type="date" className="form-control" id="dateInput" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="messageInput" className="form-label">
+                Message
+              </label>
+              <textarea
+                className="form-control"
+                id="messageInput"
+                rows="4"
+              ></textarea>
+            </div>
+            <div className="mb-3">
+              <input type="submit" value="Booking Ticket" />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModalClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
